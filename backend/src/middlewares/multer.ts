c4130c5e43eedc15e964f92b83d53400e4a22328
Sijan Bhandari <uhisijan@gmail.com>
2025-12-31 10:03:55 +0545

    feat(api) : api for profile picture uploads

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,36 @@
+import type { Request } from "express";
+import multer from "multer";
+import type { Multer } from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  console.log({ file });
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/jpg",
+    "image/webp",
+    "application/pdf",
+    "video/mp4",
+    "video/webm",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 3 * 1024 * 1024 },
+  fileFilter: fileFilter,
+});
+
+export { upload };
