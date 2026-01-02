b9b6f688879a027d615924f003748871828df56b
Loopfelloff <abhiyanregmi@gmail.com>
2026-01-02 23:04:09 +0545

    fixed the routing context validation to the dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,17 +9,13 @@ function App() {
     <div className="flex min-w-screen min-h-screen  flex-col items-center bg-white relative">
       <Routes>
         {/* Of course you can add the routes up in here as of now this is just the bare one */}
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Dashboard />} />
         <Route
           path="/pageNotFound"
           element={<h1>Hello this is the default 404 page</h1>}
         />
         <Route path="/profile" element={<Profile />} />
         <Route path="/:loginOrSignup" element={<LoginOrSignup />} />
-        <Route
-          path="/home/*"
-          element={<h1>Hello this is the home followed by something page</h1>}
-        />
         <Route path="*" element={<h1>Hello this is the default 404 page</h1>} />
       </Routes>
       <Footer />
