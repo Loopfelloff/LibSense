506b9596608456cd37766686a7e2c3be097e8133
Loopfelloff <abhiyanregmi@gmail.com>
2026-01-12 23:44:20 +0545

    fixed : the TS types for the response from the topRated section and some minor frontend issues

--- frontend/src/pages/TopRated.tsx
+++ frontend/src/pages/TopRated.tsx
@@ -5,11 +5,12 @@ import { getTopRated } from '../apis/topRated';
 import { TailSpin } from 'react-loader-spinner';
 
 type topRatedBooksType = {
-    averageRating : number; 
+    avg_book_rating : number; 
     book_cover_image? : string;
     description : string;
     book_title : string;
-    id : string
+    id : string;
+    isbn : string;
 }
 
 export function TopRated() {
@@ -89,7 +90,7 @@ export function TopRated() {
                             <svg className="w-5 h-5 text-yellow-400 fill-current" viewBox="0 0 20 20">
                               <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
                             </svg>
-                            <span className="text-gray-900 font-semibold text-sm">{book.averageRating.toFixed(1)}</span>
+                            <span className="text-gray-900 font-semibold text-sm">{book.avg_book_rating.toFixed(1)}</span>
                           </div>
                         </td>
                       </tr>
