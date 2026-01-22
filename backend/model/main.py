b9eb602c930e93478d1437d3a1f2c026ba63d9f6
Loopfelloff <abhiyanregmi@gmail.com>
2026-01-22 18:19:47 +0545

    added : more missing files have been added

--- backend/model/main.py
+++ backend/model/main.py
@@ -3,8 +3,8 @@ from .embeddUsers import router as user_router
 from .cosineDistance import router as search_router
 from .api.fastapi import app
 
-app.include_router(books_router, prefix="/books", tags=["Embedding"])
-app.include_router(user_router, prefix="/users", tags=["Embedding"])
+app.include_router(books_router, prefix="/books", tags=["Embedding"]) # these tags are used only for embedding purposes
+app.include_router(user_router, prefix="/users", tags=["Embedding"]) # these tags are useful in the localhost:8000/docs only 
 app.include_router(search_router, tags=["Search"])
 
 
